132303cdb875e36ab8e28b6ef576b86f732cb519
Dmitry Zapadnov <mitka.dev@gmail.com>
2021-03-13 15:43:40 +0300

    develop: поправить медиазпрос для формы

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,4 @@
+// Открытие и закрытие попапа
 let closeButton = document.querySelector('.popup__close-button');
 let editButton = document.querySelector('.button_type_edit');
 let popup = document.querySelector('.popup');
@@ -7,4 +8,24 @@ editButton.addEventListener('click', function (event) {
 closeButton.addEventListener('click', function (event) {
   popup.classList.remove('popup_status_opened');
 });
+// передача данных в попап
+let formProfile = document.querySelector('.popup__form');
+let nameInput = formProfile.querySelector('input[name="name"]');
+let descrInput = formProfile.querySelector('input[name="description"]');
 
+
+function formSubmitHadler() {
+
+  nameValue = nameInput.getAttribute('value');
+  descrValue = descrInput.getAttribute('value');
+  profileName = document.querySelector('.profile__name');
+  profileDescr = document.querySelector('.profile__description');
+
+
+
+}
+formSubmitHadler();
+console.log(nameValue);
+console.log(descrValue);
+console.log(profileName);
+console.log(profileDescr);
