50805e5a3e0e0898c7079bd83751923fc56cd3ac
Dmitry Zapadnov <mitka.dev@gmail.com>
2021-03-16 16:27:37 +0300

    feature: доделать форму редактирования профиля

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 let popup = document.querySelector('.popup');
-const closeBtn = popup.querySelector('.popup__close-button');
 let editBtn = document.querySelector('.button_type_edit');
+let saveBtn = popup.querySelector('.button_type_save');
+let closeBtn = popup.querySelector('.popup__close-button');
 // let addBtn = document.querySelector('.button_type_add');
 let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__Job');
+let profileJob = document.querySelector('.profile__job');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.name-input');
+let jobInput = formElement.querySelector('.job-input');
+
+
 // Открытие и закрытие попапа
 function openPopup() {
   popup.classList.add('popup_status_opened');
@@ -11,20 +17,22 @@ function openPopup() {
 function closePopup() {
   popup.classList.remove('popup_status_opened');
 }
-// Cохранение новых данных профиля
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.name-input');
-let JobInput = formElement.querySelector('.job-input');
-
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   profileName.textContent = nameInput.value;
-  profileJob.textContent = JobInput.value;
+  profileJob.textContent = jobInput.value;
   closePopup();
 }
-editBtn.addEventListener('click', openPopup);
+
+editBtn.addEventListener('click', () => {
+  openPopup();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  saveBtn.addEventListener('click', closePopup);
+})
 closeBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+
 
 // Поставить лайк карточке
 // let likeBtn = document.querySelectorAll('.button_type_like');
