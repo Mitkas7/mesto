f178d9adcf71dd8e39cbeeb59edfd7cf16a7320b
Dmitry Zapadnov <mitka.dev@gmail.com>
2021-03-18 15:45:58 +0300

    feat: добавить механизм удаления карточек

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 let popup = document.querySelector('.popup');
+let popupEdit = document.querySelector('.popup_type_edit');
+let popupAdd = document.querySelector('.popup_type_add');
 let editBtn = document.querySelector('.button_type_edit');
-let saveBtn = popup.querySelector('.button_type_save');
-let closeBtn = popup.querySelector('.popup__button-close');
-// let addBtn = document.querySelector('.button_type_add');
+let saveBtn = document.querySelector('.button_type_save');
+let closeBtn = document.querySelector('.popup__button-close');
+let addBtn = document.querySelector('.profile__button-add');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__form-item_type_name');
 let jobInput = formElement.querySelector('.popup__form-item_type_job');
-
-
-// Открытие и закрытие попапа
+// Открытие и закрытие попапа редактирования профиля
 function openPopup() {
   popup.classList.add('popup_status_opened');
   nameInput.value = profileName.textContent;
@@ -25,22 +25,45 @@ function formSubmitHandler(evt) {
   profileJob.textContent = jobInput.value;
   closePopup();
 }
-
 editBtn.addEventListener('click', () => {
   openPopup();
 })
 closeBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+// открытие и закрытие попапа добавления карточки
+function openPopupAdd() {
+  popupAdd.classList.add('popup_status_opened');
+}
+function closePopupAdd() {
+  popupAdd.classList.remove('popup_status_opened');
+}
 
+addBtn.addEventListener('click', () => {
+  openPopupAdd();
+})
+// Удалить карточку
+let placeCard = document.querySelectorAll('.place');
+let delBtn = document.querySelectorAll('.place__button-remove');
+for (let i = 0; i < delBtn.length; i++) {
+  delBtn[i].addEventListener('click', remove);
+  function remove() {
+    placeCard[i].remove();
+  }
+  for (let i = 0; i < placeCard.length; i++) {
+    function remove() {
+      placeCard[i].remove();
+    }
+}
+}
 
 // Поставить лайк карточке
-// можно не удалять а оставить закоментированным?)
-// let likeBtn = document.querySelectorAll('.button_type_like');
-// for (let i = 0; i < likeBtn.length; i++) {
-//   likeBtn[i].addEventListener('click', like);
-//   function like() {
-//     likeBtn[i].classList.toggle('button_type_like-active');
-//   }
-// }
+let likeBtn = document.querySelectorAll('.button_type_like');
+for (let i = 0; i < likeBtn.length; i++) {
+  likeBtn[i].addEventListener('click', like);
+  function like() {
+    likeBtn[i].classList.toggle('button_type_like-active');
+  }
+}
+
 
 
