b1262a837c2e735d3740b3759c1743b83373c072
Dmitry Zapadnov <mitka.dev@gmail.com>
2021-07-09 12:57:14 +0300

    dev: add preview method for Card

--- js/script.js
+++ js/script.js
@@ -1,30 +1,5 @@
-'use strict'
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://images.unsplash.com/photo-1609788802533-66a4249ca2b9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
-  },
-  {
-    name: 'Маяк Эргешельд',
-    link: 'https://images.unsplash.com/photo-1610897592260-e61cdb60f6a9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1335&q=80'
-  },
-  {
-    name: 'Алтайский край',
-    link: 'https://images.unsplash.com/photo-1494791286225-ea86fc957ba7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1047&q=80'
-  },
-  {
-    name: 'Эстосадок',
-    link: 'https://images.unsplash.com/photo-1610043169421-bec04b55ab46?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
-  },
-  {
-    name: 'Парк Рускеала',
-    link: 'https://images.unsplash.com/photo-1548288242-d454d4648b55?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80'
-  },
-  {
-    name: 'Приморский край',
-    link: 'https://images.unsplash.com/photo-1531916978893-6cdfd5df4e91?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80'
-  },
- ];
+
+import { initialCards } from "./initialcards.js";
 const cardsArray = document.querySelector('.places__cards');
 // Попапы
 const popupEdit = document.querySelector('.popup_type_edit');
@@ -88,7 +63,7 @@ popups.forEach((popup) => {
 class Card {
   constructor(cardData) {
     this.name = cardData.name;
-    this.link =  cardData.link;
+    this.link = cardData.link;
   }
   getView() {
     this.element = cardTemplate.querySelector('.place').cloneNode(true);
@@ -97,6 +72,8 @@ class Card {
     cardImage.src = this.link;
     cardImage.alt = this.name;
     cardName.textContent = this.name;
+    this.cardImage = cardImage;
+    this.cardName = cardName;
     this.setEventListeners();
     return this.element;
   }
@@ -110,29 +87,15 @@ class Card {
   }
   likeCard() {
     this.element.querySelector('.place__button-like').classList.toggle('button_type_like-active');
-    console.log(event.target);
   }
   openCardPreview() {
     openPopup(popupImage);
     popupPic.src = this.cardImage.src;
-    this.popupPic.alt = cardName.textContent;
-    this.popupName.textContent = cardName.textContent;
+    popupPic.alt = this.cardName.textContent;
+    popupName.textContent = this.cardName.textContent;
   }
 
 }
-
-
-// function createCard(cardData) {
-
-//   // Посмотреть фото
-//   cardImage.addEventListener('click', () => {
-//     openPopup(popupImage);
-//     popupPic.src = cardImage.src;
-//     popupPic.alt = cardName.textContent;
-//     popupName.textContent = cardName.textContent;
-//   })
-//   return cardItem;
-// }
 // Функция сохранения новых данных профиля
 function formSubmitHandler(evt) {
   evt.preventDefault();
