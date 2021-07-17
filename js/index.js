3fbaebc2c7610f80406a627a83ac274f155427de
Dmitry Zapadnov <mitka.dev@gmail.com>
2021-07-17 17:18:17 +0300

    fix: 2nd review. Transfer funcs and constants beetwen js files

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
-import { cardsArray } from './constants.js';
+import { cardsArray, addBtn,
+  editBtn, formAddCard, formEditProfile,
+popupAdd, popupEdit, placeName, imageUrl,
+nameInput, jobInput, profileName, profileJob} from './constants.js';
 import { Card } from './card.js';
 import { initialCards } from './initialcards.js';
+import { openPopup, closePopup } from './utils/utils.js';
 import { popupEditValidator, popupAddValidator} from './formValidator.js';
-import { createCard } from './utils/utils.js';
 // Сгенерировать начальные карты
 initialCards.forEach((data) => {
   const cardElement = createCard(data);
@@ -11,3 +14,52 @@ initialCards.forEach((data) => {
 //запуск валидатора
 popupEditValidator.enableValidation();
 popupAddValidator.enableValidation();
+// функция создания карты
+function createCard(item) {
+  const card = new Card(item, '.list-item-template');
+  const createdCard = card.getView();
+  return createdCard;
+}
+// Функция добавления карты в общий список
+function addNewCard(cardItem) {
+  cardsArray.prepend(createCard(cardItem));
+}
+// функция добавления новой карты на страницу
+function saveNewCard(evt) {
+  evt.preventDefault(evt);
+  const newCard = {
+    name: placeName.value,
+    link: imageUrl.value,
+    alt: placeName.value,
+  }
+  addNewCard(newCard);
+  closePopup(evt.target.closest('.popup_type_add'));
+  formAddCard.reset();
+}
+// Функция сохранения новых данных профиля
+function submitEditProfileForm(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup(popupEdit);
+}
+// Обработчики событий
+// для сохранения новых данных профиля
+formEditProfile.addEventListener('submit', submitEditProfileForm);
+// для открытия попапа добавления карточки
+addBtn.addEventListener('click', () => {
+  openPopup(popupAdd);
+  placeName.value = '';
+  imageUrl.value = '';
+  popupAddValidator.resetForm();
+  popupAddValidator.resetInputsErrors();
+});
+// для открытия попапа редактирования профиля
+editBtn.addEventListener('click', () => {
+  openPopup(popupEdit);
+  popupEditValidator.resetInputsErrors();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+})
+// для добавления новой карточки на страницу
+formAddCard.addEventListener('submit', saveNewCard);
--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,17 +1,10 @@
-import {
-  formEditProfile, formAddCard,
-  addBtn, editBtn, popupAdd, popupEdit,
-  nameInput, jobInput, placeName,
-  imageUrl, profileName, profileJob, cardsArray} from '.././constants.js';
-import { Card } from '../card.js';
-import { popupAddValidator, popupEditValidator } from '../formValidator.js';
 // Общая функция открытия попапов
 export function openPopup(popup) {
   popup.classList.add('popup_status_opened');
   setListener(popup);
 }
 // Общая функция закрытия попапов
-function closePopup(popup) {
+export function closePopup(popup) {
   popup.classList.remove('popup_status_opened');
   removeCloseListener(popup);
 }
@@ -42,53 +35,3 @@ popups.forEach((popup) => {
     }
   })
 });
-// функция создания карты
-export function createCard(item) {
-  const card = new Card(item, '.list-item-template');
-  const createdCard = card.getView();
-  return createdCard;
-}
-// Функция добавления карты в общий список
-export function addNewCard(cardItem) {
-  cardsArray.prepend(createCard(cardItem));
-}
-// функция добавления новой карты на страницу
-function saveNewCard(evt) {
-  evt.preventDefault(evt);
-  const newCard = {
-    name: placeName.value,
-    link: imageUrl.value,
-    alt: placeName.value,
-  }
-  addNewCard(newCard);
-  closePopup(evt.target.closest('.popup_type_add'));
-  formAddCard.reset();
-}
-// Функция сохранения новых данных профиля
-export function submitEditProfileForm(evt) {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  closePopup(popupEdit);
-}
-// Обработчики событий
-// для сохранения новых данных профиля
-formEditProfile.addEventListener('submit', submitEditProfileForm);
-
-// для открытия попапа добавления карточки
-addBtn.addEventListener('click', () => {
-  openPopup(popupAdd);
-  placeName.value = '';
-  imageUrl.value = '';
-  popupAddValidator.resetForm();
-  popupAddValidator.resetInputsErrors();
-});
-// для открытия попапа редактирования профиля
-editBtn.addEventListener('click', () => {
-  openPopup(popupEdit);
-  popupEditValidator.resetInputsErrors();
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-})
-// для добавления новой карточки на страницу
-formAddCard.addEventListener('submit', saveNewCard);
